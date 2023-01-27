66a3e4d11cf413520d2c99ac65796b8f84d0e75d
Bhuwan Tiwari <bhuwantiwari2002@gmail.com>
2023-01-27 10:12:00 -0600

    Added skill-list web componenet

---
+++ Script/skills-list.js
@@ -0,0 +1,27 @@
+window.customElements.define('skill-section', class extends HTMLElement
+{
+    
+    constructor()
+    {
+        super(); //Calling the constructor of the HTMLElement class and not the Skills 
+        const row = document.getElementById('skill-section-row'); 
+        const ColumDiv = document.createElement('div');
+        ColumDiv.classList.add('col-lg');
+        //Creating an image node
+        const image = document.createElement('img');
+        image.src = this.getAttribute('path');
+        image.style.width = "20%";
+        //Creating an heading node
+        const heading = document.createElement('h4');
+        heading.classList.add('fw-light');
+        heading.textContent = this.getAttribute('name');
+        const hr = document.createElement('hr');
+        this.parentNode.appendChild(ColumDiv);
+        ColumDiv.appendChild(image);
+        ColumDiv.appendChild(heading);
+        ColumDiv.appendChild(hr);
+    }
+   
+
+});
+
