808893f122497fc48787e78dd92ea44185a91366
Bhuwan Tiwari <bhuwantiwari2002@gmail.com>
2023-05-17 16:23:55 -0500

    Added background hover effect

--- Script/background-hover.js
+++ Script/background-hover.js
@@ -1,7 +1,17 @@
 const backgroundCursor = document.getElementById('background-mouse');
 
-backgroundCursor.addEventListener("mouseover", function(event) {
-    // Code to be executed when the mouse is moved over the element
-    
-    console.log("Mouse over event detected!");
-  });
+const timeout = setTimeout(backgroundHover(), 50000)
+function backgroundHover() {
+  document.body.onpointermove = event => {
+    let clientX = event.x;
+    let clientY = event.y;
+    // This will snap it into the center of the object
+    // backgroundCursor.style.left = clientX + "px";
+    // backgroundCursor.style.top = clientY + "px";
+
+    backgroundCursor.animate({
+      left: clientX + "px",
+      top: clientY + "px"
+    }, {duration: 3000, fill: "forwards"}); // The fill forwards doesn't reset the property
+  }
+}
