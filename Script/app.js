8180ff543a9a2fca4d61b18fefbf0e2c10e021a8
Bhuwan <bhuwantiwari2002@gmail.com>
2023-05-17 12:44:43 -0500

    Yes

--- Script/app.js
+++ Script/app.js
@@ -1,29 +1,31 @@
-let website_intro = document.querySelector('.website-intro');
-let website_intro_header = document.querySelector('.website-intro-header');
-let website_intro_logo = document.querySelectorAll('.website-intro-logo');
+/* The goal of the code below is the welcome screen, currently turning it off. Might uncomment it after I fix some code */
 
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        website_intro_logo.forEach((span,idx) => { // idx means the index
-            setTimeout(() => {
-                span.classList.add('active');
-            }, (idx +1) * 400)
-        });
+// let website_intro = document.querySelector('.website-intro');
+// let website_intro_header = document.querySelector('.website-intro-header');
+// let website_intro_logo = document.querySelectorAll('.website-intro-logo');
 
-        setTimeout(() => {
-            website_intro_logo.forEach((span,idx) => { 
-                setTimeout(() => {
-                    span.classList.remove('active');
-                    span.classList.add('fade')
-                }, (idx + 1) * 50)
-            })
-        },2000);
+// window.addEventListener('DOMContentLoaded', () => {
+//     setTimeout(() => {
+//         website_intro_logo.forEach((span,idx) => { // idx means the index
+//             setTimeout(() => {
+//                 span.classList.add('active');
+//             }, (idx +1) * 400)
+//         });
 
-        setTimeout(() => {
-            website_intro.style.top = '-100vh';
-        },2300)
-    })
-});
+//         setTimeout(() => {
+//             website_intro_logo.forEach((span,idx) => { 
+//                 setTimeout(() => {
+//                     span.classList.remove('active');
+//                     span.classList.add('fade')
+//                 }, (idx + 1) * 50)
+//             })
+//         },2000);
+
+//         setTimeout(() => {
+//             website_intro.style.top = '-100vh';
+//         },2300)
+//     })
+// });
 
 // Photography Images
 const track = document.querySelector(".photography-image-track");
