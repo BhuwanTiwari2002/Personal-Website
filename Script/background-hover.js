980077518e6785959a51e663f9a31f9c69866056
Bhuwan <bhuwantiwari2002@gmail.com>
2023-05-17 15:36:55 -0500

    Working on background cursor

---
+++ Script/background-hover.js
@@ -0,0 +1,7 @@
+const backgroundCursor = document.getElementById('background-mouse');
+
+backgroundCursor.addEventListener("mouseover", function(event) {
+    // Code to be executed when the mouse is moved over the element
+    
+    console.log("Mouse over event detected!");
+  });
